a2ec723ec185d0f5351e754c4e79b015b59dfbc7
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-24 21:01:31 +0300

    Product entity and category entity join categoryId in ProductDetail.cs for NHibernate

--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -12,13 +12,26 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal:NHEntityRepositoryBase<Product>,IProductDal
     {
+        private NHibernateHelper _nHibernateHelper;
         public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
+            _nHibernateHelper = nHibernateHelper;
         }
 
         public List<ProductDetail> GetProductDetails()
         {
-            throw new NotImplementedException();
+            using (var session = _nHibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                    join c in session.Query<Category>() on p.CategoryId equals c.CategoryId
+                    select new ProductDetail
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        CategoryName = c.CategoryName
+                    };
+                return result.ToList();
+            }
         }
     }
 }
