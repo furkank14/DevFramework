8f0ac6e8d17b2505ef4576701ec07b5023d9c2c6
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-24 18:31:36 +0300

    DataAccess layer implement NHibernate

---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using DevFramework.Core.DataAccess.NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
+{
+    public class SqlServerHelper:NHibernateHelper
+    {
+        protected override ISessionFactory InitializeFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012.ConnectionString(c=>c.FromConnectionStringWithKey("NorthwindContext"))).Mappings(t=>t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+        }
+    }
+}
---
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
+{
+    class NhProductDal:NHEntityRepositoryBase<Product>,IProductDal
+    {
+        public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
