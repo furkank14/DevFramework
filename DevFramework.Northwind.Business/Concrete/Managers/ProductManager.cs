aceac8773b340dfa48956667d180fc2dd22eb2e3
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-24 23:47:11 +0300

    Product procress in Business layer Fluent Validator added

---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+
+namespace DevFramework.Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün İsmi Boş Olamaz");
+            RuleFor(p => p.ProductName).Length(3, 20);
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.QuantityPerUnit).Empty().When(p => p.ProductName == "Bos");
+            RuleFor(p => p.UnitPrice).NotEmpty();
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+
+        }
+    }
+}
